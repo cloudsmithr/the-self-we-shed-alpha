0d875bbfc91d071aaf6d2cb1ec76c52e3dfe33a3
Ryan <ryan@rsmith.cloud>
2026-03-04 12:07:07 -0700

    fixing some stuff related to prisma on dev machine

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
+console.log('DB URL: ' + process.env['DATABASE_URL']);
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
