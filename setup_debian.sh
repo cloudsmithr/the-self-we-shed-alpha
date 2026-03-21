677fdbd3f3324c738951f139152ab77ef9ef62da
Ryan Smith <rsmith.cloud>
2026-03-20 22:01:45 -0600

    fixing setup scripts

--- setup_debian.sh
+++ setup_debian.sh
@@ -104,7 +104,7 @@ else
   # Official Docker apt repo
   sudo apt update
   sudo apt install -y ca-certificates curl
-  sudo install -y -m 0755 -d /etc/apt/keyrings
+  sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
